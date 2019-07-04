d9fbd6151caf01789dcdbeef14e996692f3cfe2e
Ekansh Gupta <33174166+eKuG@users.noreply.github.com>
2019-07-05 03:08:29 +0530

    Add files via upload

--- drowsiness_detection.py
+++ drowsiness_detection.py
@@ -30,3 +30,88 @@ def eye_aspect_ratio(eye):
  
 	# return the eye aspect ratio
 	return ear
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--shape-predictor", required=True,
+	help="shape_predictor_68_face_landmarks.dat")
+ap.add_argument("-a", "--alarm", type=str, default="",
+	help="sound.mp3")
+ap.add_argument("-w", "--webcam", type=int, default=0,
+	help="0")
+args = vars(ap.parse_args())
+
+EYE_AR_THRESH = 0.3
+EYE_AR_CONSEC_FRAMES = 48
+
+COUNTER = 0
+ALARM_ON = False
+
+print("[INFO] loading facial landmark predictor...")
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(args["shape_predictor"])
+
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+print("[INFO] starting video stream thread...")
+vs = VideoStream(src=args["webcam"]).start()
+time.sleep(1.0)
+ 
+while True:
+
+	frame = vs.read()
+	frame = imutils.resize(frame, width=450)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+	rects = detector(gray, 0)
+
+
+	for rect in rects:
+
+		shape = predictor(gray, rect)
+		shape = face_utils.shape_to_np(shape)
+ 
+		leftEye = shape[lStart:lEnd]
+		rightEye = shape[rStart:rEnd]
+		leftEAR = eye_aspect_ratio(leftEye)
+		rightEAR = eye_aspect_ratio(rightEye)
+ 
+		ear = (leftEAR + rightEAR) / 2.0
+        
+		leftEyeHull = cv2.convexHull(leftEye)
+		rightEyeHull = cv2.convexHull(rightEye)
+		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+		cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+        
+		if ear < EYE_AR_THRESH:
+			COUNTER += 1
+
+			if COUNTER >= EYE_AR_CONSEC_FRAMES:
+
+				if not ALARM_ON:
+					ALARM_ON = True
+
+					if args["alarm"] != "":
+						t = Thread(target=sound_alarm,
+							args=(args["alarm"],))
+						t.deamon = True
+						t.start()
+
+				cv2.putText(frame, "DROWSINESS ALERT!", (10, 30),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+		else:
+			COUNTER = 0
+			ALARM_ON = False
+
+		cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+ 
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+ 
+	if key == ord("q"):
+		break
+
+cv2.destroyAllWindows()
+vs.stop()
